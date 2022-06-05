db91c86f56144eb43ea7cab91914b362fccdfe9e
pepelaz <pepelaz101@gmail.com>
2022-06-05 10:48:44 +0700

    mockup is ready

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -5,26 +5,51 @@ import "hardhat/console.sol";
 import "./Erc20Token.sol";
 
 contract Dao {
-   
     Erc20Token public immutable token;
 
-    constructor(address _address) {
-        token = Erc20Token(_address);
+    address public immutable chairman;
+
+    uint256 public immutable minQuorum;
+
+    uint256 public immutable duration;
+
+    mapping(address => uint256) deposits;
+
+
+    struct Proposal {
+        string text;
+        uint256 amount;
+        uint256 start;
     }
 
-    function addProposal() public {
+    mapping(string => Proposal) proposals;
+
+
+    constructor(address _chairman, address _token, uint256 _minQuorum, uint256 _duration) {
+        chairman = _chairman;
+        token = Erc20Token(_token);
+        minQuorum = _minQuorum;
+        duration = _duration;
+    }
+
+    function addProposal(string memory _text, string memory _desc) public {
+        require(msg.sender == chairman, "only chairman can add proposals");
+        proposals[_desc] = Proposal({text: _text, amount: 0, start: block.timestamp});
+    }
+
+    function deposit(uint256 _amount) public {
+        token.transferFrom(msg.sender, address(this), _amount);
+        deposits[msg.sender] += _amount;
 
     }
 
-    function deposit() public {
-
+    function vote(string memory _desc) public {
+        proposals[_desc].amount += deposits[msg.sender];
     }
 
-    function vote() public {
-        
-    }
+    function finishProposal(string memory _desc) public {
+        require(block.timestamp >= proposals[_desc].start + duration, "proposal is not over yet");
+        console.log(proposals[_desc].amount);
 
-    function finish() public {
-        
     }
 }
--- test/Dao.ts
+++ test/Dao.ts
@@ -1,8 +1,9 @@
 import { isCommunityResourcable } from "@ethersproject/providers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { BigNumber } from "ethers";
 const { parseEther } = ethers.utils;
+const { MaxUint256 } = ethers.constants;
 
 
 
@@ -14,28 +15,87 @@ describe("Dao", function () {
 
   let acc3: any;
 
+  let acc4: any;
+
   let token: any;
 
   let dao: any;
 
   beforeEach(async function() {
-    [acc1, acc2, acc3] = await ethers.getSigners()
+    [acc1, acc2, acc3, acc4] = await ethers.getSigners()
 
     // deploy ERC20 token
     const Erc20Token = await ethers.getContractFactory('Erc20Token', acc1)
-    token = await Erc20Token.deploy("Pepelaz","PPLZ", ethers.utils.parseEther("10000"))
+    token = await Erc20Token.deploy("Pepelaz","PPLZ", parseEther("10000"))
     await token.deployed()  
     
     // deploy Dao contract
     const Dao = await ethers.getContractFactory('Dao', acc1)
-    dao = await Dao.deploy(token.address)
+    dao = await Dao.deploy(acc1.address, token.address, 3, 60*60*24*3)
     await dao.deployed()  
 
-   
+    token.mint(acc2.address, parseEther('10000'))
+    token.connect(acc2).approve(dao.address, MaxUint256)
+
+    token.mint(acc3.address, parseEther('10000'))
+    token.connect(acc3).approve(dao.address, MaxUint256)
+
+    token.mint(acc4.address, parseEther('10000'))
+    token.connect(acc4).approve(dao.address, MaxUint256)
   })
 
   it("should be deployed", async function(){
      expect(dao.address).to.be.properAddress
   })
+
+  it("test", async function(){
+    let tx = await dao.connect(acc2).deposit(parseEther('100'))
+    await tx.wait()
+
+    tx = await dao.connect(acc3).deposit(parseEther('200'))
+    await tx.wait()
+
+    tx = await dao.connect(acc2).deposit(parseEther('200'))
+    await tx.wait()
+
+    tx = await dao.connect(acc4).deposit(parseEther('500'))
+    await tx.wait()
+
+    //--------
+
+    tx = await dao.addProposal('function 1', 'description 1')
+    await tx.wait()
+
+    tx = await dao.addProposal('function 2', 'description 2')
+    await tx.wait()
+
+    tx = await dao.addProposal('function 3', 'description 3')
+    await tx.wait()
+
+    //--------
+
+    tx = await dao.connect(acc2).vote('description 1')
+    await tx.wait()
+
+    tx = await dao.connect(acc2).vote('description 2')
+    await tx.wait()
+
+    tx = await dao.connect(acc3).vote('description 1')
+    await tx.wait()
+
+    tx = await dao.connect(acc3).vote('description 3')
+    await tx.wait()
+
+    tx = await dao.connect(acc4).vote('description 3')
+    await tx.wait()
+
+    //--------
+
+    await network.provider.send("evm_increaseTime", [60*60*24*3]) 
+
+    tx = await dao.finishProposal('description 1')
+    await tx.wait()
+
+ })
   
 });
